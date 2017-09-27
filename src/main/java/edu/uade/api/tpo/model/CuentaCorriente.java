19ddc1b62af4aa16351e13b3038178471614a8ed
Agustin Battaglino <mundobaton@gmail.com>
2017-09-27 20:04:45 -0300

    add missing properties and diagram changes

--- src/main/java/edu/uade/api/tpo/model/CuentaCorriente.java
+++ src/main/java/edu/uade/api/tpo/model/CuentaCorriente.java
@@ -1,5 +1,46 @@
 package edu.uade.api.tpo.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CuentaCorriente {
 
+    private float saldo;
+    private List<Transaccion> transacciones;
+
+    public CuentaCorriente() {
+        this.transacciones = new ArrayList<>();
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public List<Transaccion> getTransacciones() {
+        return transacciones;
+    }
+
+    public void setTransacciones(List<Transaccion> transacciones) {
+        this.transacciones = transacciones;
+    }
+
+    public void addTransaccion(Transaccion transaccion) {
+        this.transacciones.add(transaccion);
+    }
+
+    public void removeTransaccion(Transaccion transaccion) {
+        this.transacciones.remove(transaccion);
+    }
+
+    public boolean hasTransacciones() {
+        return !this.transacciones.isEmpty();
+    }
+
+    public boolean hasTransaccion(Transaccion transaccion) {
+        return this.transacciones.contains(transaccion);
+    }
 }
