db6993070e3762c913c98651ae036251e8881ef5
Agustin Battaglino <mundobaton@gmail.com>
2017-09-30 22:26:03 -0300

    wip

--- src/main/java/edu/uade/api/tpo/dao/impl/ServicioDaoImpl.java
+++ src/main/java/edu/uade/api/tpo/dao/impl/ServicioDaoImpl.java
@@ -3,6 +3,7 @@ package edu.uade.api.tpo.dao.impl;
 import edu.uade.api.tpo.dao.AbstractDao;
 import edu.uade.api.tpo.dao.GenericDao;
 import edu.uade.api.tpo.model.Servicio;
+import edu.uade.api.tpo.model.TipoContratacion;
 import edu.uade.api.tpo.util.UUIDUtils;
 
 import java.sql.Connection;
@@ -41,16 +42,35 @@ public class ServicioDaoImpl extends AbstractDao<Servicio> {
         ps.setString(3, servicio.getDescripcion());
         ps.setString(4, servicio.toImagesTokenized());
         ps.setString(5, servicio.toCertificadosTokenized());
+        ps.setString(6, servicio.getContratacion().toString());
         return ps;
     }
 
     @Override
     public PreparedStatement update(Servicio servicio, Connection conn) throws SQLException {
-        return null;
+        String query = "UPDATE " + schema + ".servicios SET nombre = ?, descripcion = ?, imagenes = ?, certificados = ?, tipo_contratacion = ? WHERE servicio_id = ?";
+        PreparedStatement ps = conn.prepareStatement(query);
+        ps.setString(1, servicio.getNombre());
+        ps.setString(2, servicio.getDescripcion());
+        ps.setString(3, servicio.toImagesTokenized());
+        ps.setString(4, servicio.toCertificadosTokenized());
+        ps.setString(5, servicio.getContratacion().toString());
+        ps.setString(6, servicio.getId());
+        return ps;
     }
 
     @Override
     public Servicio map(ResultSet rs) throws SQLException {
-        return null;
+        Servicio servicio = null;
+        if (rs.first()) {
+            servicio = new Servicio();
+            servicio.setId(rs.getString("servicio_id"));
+            servicio.setNombre(rs.getString("nombre"));
+            servicio.setDescripcion(rs.getString("descripcion"));
+            servicio.fromImagesTokenized(rs.getString("imagenes"));
+            servicio.fromCertificadosTokenized(rs.getString("certificados"));
+            servicio.setContratacion(TipoContratacion.valueOf(rs.getString("tipo_contratacion")));
+        }
+        return servicio;
     }
 }
