0446b7df60e3aecfd4645c9572ca2e3acd8a3c0e
Mariel Melaragno <mmelaragno@despegar.com>
2017-10-02 19:22:27 -0300

    agrego los metodos vacios del factory

--- src/main/java/edu/uade/api/tpo/model/ArticuloFactory.java
+++ src/main/java/edu/uade/api/tpo/model/ArticuloFactory.java
@@ -1,5 +1,16 @@
 package edu.uade.api.tpo.model;
+import com.mysql.cj.api.xdevapi.Collection;
+
+
 
 public class ArticuloFactory {
+	
+	public void crearProducto(String nombre, String descripcion, Collection imagenes, int periodoGarantia, TipoPeriodo tipo ) {
+		
+	}
+	
+	public void crearServicio(String nombre, String descripcion, Collection imagenes, Collection certificados, TipoContratacion tipo) {
+		
+	}
 
 }
