5070abcf4f5c5e40f8b6d64f0ffd6baefc28e01a
Martin Sacco <sacco.martin@gmail.com>
2017-10-06 20:57:50 -0300

    Login: validation and connected to system

--- src/main/java/edu/uade/api/tpo/ui/IniciarSesion.java
+++ src/main/java/edu/uade/api/tpo/ui/IniciarSesion.java
@@ -10,12 +10,15 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent;
+import edu.uade.api.tpo.model.SistemaUsuarios;
 
 public class IniciarSesion {
 
 	private JFrame frmIniciarSesion;
-	private JTextField textField;
+	private JTextField nombreUsuarioField;
 	private JPasswordField passwordField;
 
 	/**
@@ -52,51 +55,113 @@ public class IniciarSesion {
 		frmIniciarSesion.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
+		nombreUsuarioField = new JTextField();
+		passwordField = new JPasswordField();
+		JButton btnAceptar = new JButton("Ingresar");
+		JButton btnCancelar = new JButton("Cancelar");
+		JLabel notification = new JLabel("");
+		
+		
+		
 		panel.setBackground(new Color(255, 255, 224));
 		panel.setBounds(0, 0, 450, 278);
 		frmIniciarSesion.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblNombreDeUsuario = new JLabel("Nombre de usuario:");
-		lblNombreDeUsuario.setBounds(92, 58, 103, 14);
+		lblNombreDeUsuario.setBounds(92, 58, 150, 14);
 		panel.add(lblNombreDeUsuario);
 		
 		JLabel lblContrasea = new JLabel("Contrase\u00F1a: ");
-		lblContrasea.setBounds(92, 101, 79, 14);
+		lblContrasea.setBounds(92, 101, 150, 14);
 		panel.add(lblContrasea);
 		
-		textField = new JTextField();
-		textField.setBounds(209, 55, 134, 20);
-		panel.add(textField);
-		textField.setColumns(10);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBounds(209, 98, 134, 20);
-		panel.add(passwordField);
-		
-		JButton btnAceptar = new JButton("Aceptar");
-		btnAceptar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				MenuPrincipal menu = new MenuPrincipal();
-				menu.setVisible(true);
-				frmIniciarSesion.dispose();
+		nombreUsuarioField.setBounds(220, 55, 134, 20);
+		nombreUsuarioField.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {}
+			public void keyTyped(KeyEvent e) {}
+			public void keyReleased(KeyEvent e) {
+				validateForm(btnAceptar, nombreUsuarioField.getText(), new String(passwordField.getPassword()));
 			}
 		});
-		btnAceptar.setBounds(81, 168, 103, 23);
+		panel.add(nombreUsuarioField);
+		nombreUsuarioField.setColumns(10);
+		
+		
+		passwordField.setBounds(220, 98, 134, 20);
+		passwordField.addKeyListener(new KeyListener() {
+			public void keyPressed(KeyEvent e) {}
+			public void keyTyped(KeyEvent e) {}
+			public void keyReleased(KeyEvent e) {
+				validateForm(btnAceptar, nombreUsuarioField.getText(), new String(passwordField.getPassword()));
+			}
+		});
+		panel.add(passwordField);
+		
+		
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnAceptar.setEnabled(false);
+				notification.setText("");
+				
+				// TODO: REMOVE THIS
+				MenuPrincipal amenu = new MenuPrincipal();
+				amenu.setVisible(true);
+				frmIniciarSesion.dispose();
+				
+				boolean loginSuccess = login(nombreUsuarioField.getText(), new String(passwordField.getPassword())); 
+				if (loginSuccess) {
+					MenuPrincipal menu = new MenuPrincipal();
+					menu.setVisible(true);
+					frmIniciarSesion.dispose();
+				} else {
+					notification.setText("Usuario o Password no valido");
+				}
+				btnAceptar.setEnabled(true);
+			}
+		});
+		btnAceptar.setEnabled(false);
+		btnAceptar.setBounds(81, 168, 150, 23);
 		panel.add(btnAceptar);
 		
-		JButton btnCancelar = new JButton("Cancelar");
+		
 		btnCancelar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				notification.setText("");
 				OpcionIngreso opcion = new OpcionIngreso();
 				opcion.setVisible(true);
 				frmIniciarSesion.dispose();
 			}
 		});
-		btnCancelar.setBounds(252, 168, 103, 23);
+		btnCancelar.setBounds(252, 168, 150, 23);
 		panel.add(btnCancelar);
+		
+		notification.setBounds(92, 220, 300, 14);
+		panel.add(notification);
 	}
+	
 	public void setVisible(boolean isVisible) {
 		this.frmIniciarSesion.setVisible(isVisible);
+	}
+	
+	public boolean login(String nombreUsuario, String password) {
+		System.out.println("Trying to login with credentials: " + nombreUsuario);
+		SistemaUsuarios su = SistemaUsuarios.getInstance();
+		try {
+			System.out.println(su.login(nombreUsuario, password).getNombre());
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+		
+	}
+	
+	public void validateForm(JButton acceptButton, String nombreUsuario, String password) {
+		if (nombreUsuario.length() > 0 && password.length() > 0) {
+			acceptButton.setEnabled(true);
+		} else {
+			acceptButton.setEnabled(false);
+		}
 	}
 }
