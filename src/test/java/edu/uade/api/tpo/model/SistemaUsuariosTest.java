b24347ab5624ab7943cfdce8a10990147eccd2ac
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2017-10-07 20:30:38 -0300

    test sistema usuarios

--- src/test/java/edu/uade/api/tpo/model/SistemaUsuariosTest.java
+++ src/test/java/edu/uade/api/tpo/model/SistemaUsuariosTest.java
@@ -1,5 +1,6 @@
 package edu.uade.api.tpo.model;
 
+import edu.uade.api.tpo.exceptions.InvalidPasswordException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,6 +35,41 @@ public class SistemaUsuariosTest {
             u.setNombre("Flor");
             u.setApellido("Otero");
             sistemaUsuarios.modificarUsuario(u);
+        } catch (Exception e) {
+            Assert.fail("Should not throw any exception");
+        }
+    }
+
+    @Test
+    public void test_eliminarUsuario() {
+        try {
+            sistemaUsuarios.eliminarUsuario("flor");
+            Usuario u = sistemaUsuarios.buscarUsuario("flor");
+            Assert.assertNull(u);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail("Should not throw any exception");
+        }
+    }
+
+    @Test
+    public void test_validarPasswordOk() {
+        String password = "unPasswordOk1";
+        try {
+            sistemaUsuarios.validarPassword(password);
+        } catch (Exception e) {
+            Assert.fail("Should not throw any exception");
+        }
+    }
+
+    @Test
+    public void test_validarPasswordErr() {
+        String password = "unpassword";
+        try {
+            sistemaUsuarios.validarPassword(password);
+            Assert.fail("Should throw exception since password is invalid");
+        } catch (InvalidPasswordException e) {
+            Assert.assertEquals("La contraseña ingresada es incorrecta.\nLa misma debe tener entre 8 y 20 caracteres, al menos un número y al menos una letra mayúscula.", e.getMessage());
         } catch (Exception e) {
             Assert.fail("Should not throw any exception");
         }
