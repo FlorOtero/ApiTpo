1bd544d643ffe8914aa5700c14dfa6f7f7c274a5
Erika Nuñez <erikannunez@gmail.com>
2017-10-08 23:51:22 -0300

    fix nombre usuario

--- src/main/java/edu/uade/api/tpo/model/CompraEfectivo.java
+++ src/main/java/edu/uade/api/tpo/model/CompraEfectivo.java
@@ -13,9 +13,10 @@ public class CompraEfectivo extends Transaccion {
 		//restamos de la cta cte del comprador y sumamos en la del vendedor
 		//+ comisiones
 		float saldoComprador = this.getContraparte().getCuentaCorriente().getSaldo();
-		String nombreVendedor = this.getPublicacion().getUsuarioId();
-		Usuario vendedor = SistemaUsuarios.getInstance().buscarUsuario(nombreVendedor);
+		String idVendedor = this.getPublicacion().getUsuarioId();
+		Usuario vendedor = SistemaUsuarios.getInstance().buscarUsuarioById(idVendedor);
 		float saldoVendedor = vendedor.getCuentaCorriente().getSaldo();
+		
 		//restamos de la cta cte comprador el valor de la publicacion
 		saldoComprador = saldoComprador - this.getPublicacion().getPrecio();
 		//sumamos de la cta cte vendedor el valor de la venta
@@ -24,7 +25,7 @@ public class CompraEfectivo extends Transaccion {
 		saldoVendedor = saldoVendedor - (this.getPublicacion().getPrecio() * this.getPublicacion().getComision());
 		
 		SistemaUsuarios.getInstance().buscarUsuario(this.getContraparte().getNombreUsuario()).getCuentaCorriente().setSaldo(saldoComprador);
-		SistemaUsuarios.getInstance().buscarUsuario(nombreVendedor).getCuentaCorriente().setSaldo(saldoVendedor);
+		SistemaUsuarios.getInstance().buscarUsuario(vendedor.getNombreUsuario()).getCuentaCorriente().setSaldo(saldoVendedor);
 
 	}
 }
