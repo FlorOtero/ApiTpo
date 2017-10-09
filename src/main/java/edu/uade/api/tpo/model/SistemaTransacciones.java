81f26be3afe0f1eb145f1664e063cec4d9b5584c
Erika Nuñez <erikannunez@gmail.com>
2017-10-08 23:57:38 -0300

    emprolijado de crearTransaccion

--- src/main/java/edu/uade/api/tpo/model/SistemaTransacciones.java
+++ src/main/java/edu/uade/api/tpo/model/SistemaTransacciones.java
@@ -25,27 +25,23 @@ public class SistemaTransacciones {
 		//todo: cambiar estado publicacion y transaccion despues de recibir la respuesta
 		switch (medioDePago) {
 		case EFECTIVO:
+			publicacion.setEstado(Estado.I);
 			tr = new CompraEfectivo(publicacion, contraparte);
 			break;
 		case TRANSFERENCIA_BANCARIA:
+			publicacion.setEstado(Estado.P);
 			tr = new CompraTransferenciaBancaria(publicacion, contraparte);
 			//	SistemaNotificacionCobro.getInstance()...
 			break;
 		case TARJETA_CREDITO:
+			publicacion.setEstado(Estado.P);
 			tr = new CompraTarjetaCredito(publicacion, contraparte);
 			//	SistemaNotificacionCobro.getInstance()...
 			break;
 		}
 		
 		transacciones.add(tr);
-
-		if(medioDePago.equals(MedioPago.EFECTIVO)) {
-			//Si es en efvo, inmediatamente pasa a estar inactiva
-			SistemaPublicaciones.getInstance().buscarPublicacion(publicacion.getId()).setEstado(Estado.I);
-		}else {
-			//pausamos la publicacion hasta tener novedades de la transferencia/tc
-			SistemaPublicaciones.getInstance().buscarPublicacion(publicacion.getId()).setEstado(Estado.P);
-		}
+		SistemaPublicaciones.getInstance().modificarPublicacion(publicacion);
 	}
 
 }
