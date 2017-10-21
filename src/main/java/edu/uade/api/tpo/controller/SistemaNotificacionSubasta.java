50df44e509132af375ceea950a32bb18ffa7a0b6
Agustin Battaglino <agustin.battaglino@mercadolibre.com>
2017-10-21 19:34:27 -0300

    remove unnecesary code

--- src/main/java/edu/uade/api/tpo/controller/SistemaNotificacionSubasta.java
+++ src/main/java/edu/uade/api/tpo/controller/SistemaNotificacionSubasta.java
@@ -1,29 +1,17 @@
 package edu.uade.api.tpo.controller;
 
-import edu.uade.api.tpo.dao.impl.SubastaDaoImpl;
 import edu.uade.api.tpo.model.Subasta;
 import edu.uade.api.tpo.model.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
-import java.util.List;
-
 public class SistemaNotificacionSubasta {
 
     private static SistemaNotificacionSubasta instance = null;
     private static final Logger logger = LoggerFactory.getLogger(SistemaNotificacionSubasta.class);
-    private SubastaDaoImpl subastaDao;
-    private List<Subasta> subastasActivas;
 
     private SistemaNotificacionSubasta() {
-        this.subastaDao = SubastaDaoImpl.getInstance();
-        try {
-            logger.info("Cargando todas las subastas activas...");
-            this.subastasActivas = subastaDao.findAll();
-        } catch (SQLException e) {
-            logger.error("Error buscando todas las subastas", e);
-        }
+
     }
 
     public static SistemaNotificacionSubasta getInstance() {
