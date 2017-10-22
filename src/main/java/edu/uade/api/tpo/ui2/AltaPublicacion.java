293897868ac2833cec8e0798e5c8e2a06d599f88
Erika Nuñez <erikannunez@gmail.com>
2017-10-22 16:36:48 -0300

    alta de publicacion

--- src/main/java/edu/uade/api/tpo/ui2/AltaPublicacion.java
+++ src/main/java/edu/uade/api/tpo/ui2/AltaPublicacion.java
@@ -17,13 +17,30 @@ import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.JCheckBox;
+import java.awt.CardLayout;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class AltaPublicacion {
+public class AltaPublicacion implements ItemListener{
 
+	final static String COMPRA_INMEDIATA = "Compra inmediata";
+	final static String SUBASTA = "Subasta";
+	final static String PRODUCTO = "Producto";
+	final static String SERVICIO = "Servicio";
+	
 	private JFrame frmNuevaPublicacion;
 	private JTextField txtTitulo;
 	private JPanel panelTipoPublicacion;
 	private JPanel panelCategoria;
+	private JTextField txtPrecio;
+	private JTextField txtPrecioInicial;
+	private JTextField textField;
+	private JTextField txtGarantia;
+	private JTextField txtCertificados;
 
 	/**
 	 * Launch the application.
@@ -128,15 +145,77 @@ public class AltaPublicacion {
 		lblTipoDePublicacion.setBounds(10, 360, 360, 16);
 		frmNuevaPublicacion.getContentPane().add(lblTipoDePublicacion);
 		
-		String[] tipoPublicacionStrings = { "Compra inmediata", "Subasta"};
+		String[] tipoPublicacionStrings = { COMPRA_INMEDIATA, SUBASTA};
 		JComboBox comboBoxTipoPublicacion = new JComboBox(tipoPublicacionStrings);
 		comboBoxTipoPublicacion.setBounds(10, 380, 360, 27);
 		comboBoxTipoPublicacion.setSelectedIndex(0);
+		comboBoxTipoPublicacion.addItemListener(this);
 		frmNuevaPublicacion.getContentPane().add(comboBoxTipoPublicacion);
 		
 		panelTipoPublicacion = new JPanel();
 		panelTipoPublicacion.setBounds(10, 410, 360, 180);
 		frmNuevaPublicacion.getContentPane().add(panelTipoPublicacion);
+		panelTipoPublicacion.setLayout(new CardLayout(0, 0));
+		
+		JPanel panelCompraInmediata = new JPanel();
+		panelTipoPublicacion.add(panelCompraInmediata, COMPRA_INMEDIATA);
+		panelCompraInmediata.setLayout(null);
+		
+		JLabel lblPrecio = new JLabel("Precio:");
+		lblPrecio.setBounds(0, 10, 360, 16);
+		panelCompraInmediata.add(lblPrecio);
+		
+		txtPrecio = new JTextField();
+		txtPrecio.setBounds(0, 30, 180, 30);
+		panelCompraInmediata.add(txtPrecio);
+		txtPrecio.setColumns(10);
+		
+		JLabel lblPesosArgentinos = new JLabel("pesos argentinos");
+		lblPesosArgentinos.setBounds(190, 30, 170, 30);
+		panelCompraInmediata.add(lblPesosArgentinos);
+		
+		JPanel panelSubasta = new JPanel();
+		panelTipoPublicacion.add(panelSubasta, SUBASTA);
+		panelSubasta.setLayout(null);
+		
+		JLabel lblPrecioInicial = new JLabel("Precio inicial:");
+		lblPrecioInicial.setBounds(0, 10, 90, 30);
+		panelSubasta.add(lblPrecioInicial);
+		
+		txtPrecioInicial = new JTextField();
+		txtPrecioInicial.setBounds(100, 10, 120, 30);
+		panelSubasta.add(txtPrecioInicial);
+		txtPrecioInicial.setColumns(10);
+		
+		JLabel lblPesosArgentinos_1 = new JLabel("pesos argentinos");
+		lblPesosArgentinos_1.setBounds(225, 10, 130, 30);
+		panelSubasta.add(lblPesosArgentinos_1);
+		
+		JLabel lblPrecioMinimo = new JLabel("Precio mínimo:");
+		lblPrecioMinimo.setBounds(0, 50, 100, 30);
+		panelSubasta.add(lblPrecioMinimo);
+		
+		textField = new JTextField();
+		textField.setBounds(100, 50, 120, 30);
+		panelSubasta.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblPesosArgentinos_2 = new JLabel("pesos argentinos");
+		lblPesosArgentinos_2.setBounds(225, 50, 130, 30);
+		panelSubasta.add(lblPesosArgentinos_2);
+		
+		JLabel lblVigencia = new JLabel("Vigencia:");
+		lblVigencia.setBounds(0, 90, 90, 30);
+		panelSubasta.add(lblVigencia);
+		
+		String[] vigenciaStrings = { "30", "60", "90"};
+		JComboBox comboBoxVigencia = new JComboBox(vigenciaStrings);
+		comboBoxVigencia.setBounds(100, 90, 120, 30);
+		panelSubasta.add(comboBoxVigencia);
+		
+		JLabel lblDias = new JLabel("días");
+		lblDias.setBounds(225, 90, 130, 30);
+		panelSubasta.add(lblDias);
 		
 		JSeparator separator_2 = new JSeparator();
 		separator_2.setOrientation(SwingConstants.VERTICAL);
@@ -147,14 +226,67 @@ public class AltaPublicacion {
 		lblCategoria.setBounds(410, 360, 360, 16);
 		frmNuevaPublicacion.getContentPane().add(lblCategoria);
 		
-		String[] categoriaStrings = { "Producto", "Servicio"};
+		String[] categoriaStrings = { PRODUCTO, SERVICIO };
 		JComboBox comboBoxCategoria = new JComboBox(categoriaStrings);
 		comboBoxCategoria.setBounds(410, 380, 360, 27);
+		comboBoxCategoria.addItemListener(this);
 		frmNuevaPublicacion.getContentPane().add(comboBoxCategoria);
 		
 		panelCategoria = new JPanel();
 		panelCategoria.setBounds(410, 410, 360, 180);
 		frmNuevaPublicacion.getContentPane().add(panelCategoria);
+		panelCategoria.setLayout(new CardLayout(0, 0));
+		
+		JPanel panelProducto = new JPanel();
+		panelCategoria.add(panelProducto, PRODUCTO);
+		panelProducto.setLayout(null);
+		
+		JLabel lblGarantia = new JLabel("Garantía:");
+		lblGarantia.setBounds(0, 10, 70, 30);
+		panelProducto.add(lblGarantia);
+		
+		txtGarantia = new JTextField();
+		txtGarantia.setBounds(70, 10, 110, 30);
+		panelProducto.add(txtGarantia);
+		txtGarantia.setColumns(10);
+		
+		String[] garantiaStrings = { "Meses", "Años"};
+		JComboBox comboBoxGarantia = new JComboBox(garantiaStrings);
+		comboBoxGarantia.setBounds(190, 11, 170, 30);
+		panelProducto.add(comboBoxGarantia);
+		
+		JPanel panelServicio = new JPanel();
+		panelCategoria.add(panelServicio, SERVICIO);
+		panelServicio.setLayout(null);
+		
+		JLabel lblTipoDeContratacion = new JLabel("Tipo de Contratación:");
+		lblTipoDeContratacion.setBounds(0, 10, 140, 30);
+		panelServicio.add(lblTipoDeContratacion);
+		
+		String[] contratacionStrings = { "Abono", "Única"};
+		JComboBox comboBoxTipoContratacion = new JComboBox(contratacionStrings);
+		comboBoxTipoContratacion.setBounds(150, 10, 210, 30);
+		panelServicio.add(comboBoxTipoContratacion);
+		
+		JLabel lblCertificados = new JLabel("Certificados:");
+		lblCertificados.setBounds(0, 50, 360, 16);
+		panelServicio.add(lblCertificados);
+		
+		txtCertificados = new JTextField();
+		txtCertificados.setBounds(0, 70, 200, 30);
+		panelServicio.add(txtCertificados);
+		txtCertificados.setColumns(10);
+		
+		JButton btnCargarCertificado = new JButton("Cargar certificado");
+		btnCargarCertificado.setBounds(210, 70, 150, 30);
+		panelServicio.add(btnCargarCertificado);
+		
+		JTextPane textPaneCertificados = new JTextPane();
+		textPaneCertificados.setText("Acá se tienen que ir mostrando los certificados cargados");
+		textPaneCertificados.setBackground(SystemColor.window);
+		textPaneCertificados.setEditable(false);
+		textPaneCertificados.setBounds(0, 110, 360, 60);
+		panelServicio.add(textPaneCertificados);
 		
 		JLabel lblMedioDePago = new JLabel("Medio de Pago aceptados:");
 		lblMedioDePago.setBounds(10, 600, 760, 16);
@@ -188,9 +320,20 @@ public class AltaPublicacion {
 		btnCancelar.setBounds(530, 700, 120, 30);
 		frmNuevaPublicacion.getContentPane().add(btnCancelar);
 		
-		
-	
+	}
 
+	@Override
+	public void itemStateChanged(ItemEvent e) {
 		
+		CardLayout cl;
+		String item = (String) e.getItem();
+		
+		if(item.equals(SUBASTA) || item.equals(COMPRA_INMEDIATA)) {
+			cl = (CardLayout)(panelTipoPublicacion.getLayout());
+	        cl.show(panelTipoPublicacion, item);
+		} else if(item.equals(PRODUCTO) || item.equals(SERVICIO)) {
+			cl = (CardLayout)(panelCategoria.getLayout());
+	        cl.show(panelCategoria, item);
+		}
 	}
 }
